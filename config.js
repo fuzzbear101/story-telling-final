cc9d1132544657f4dd0e0945cd2886003bb2f7d4
fuzzbear101 <81325736+fuzzbear101@users.noreply.github.com>
2025-01-29 19:05:02 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwYXsHdC3LPZ5lLdZ27mcHK83IzdrFHTM",
+  authDomain: "database-3-231af.firebaseapp.com",
+  databaseURL: "https://database-3-231af-default-rtdb.firebaseio.com",
+  projectId: "database-3-231af",
+  storageBucket: "database-3-231af.appspot.com",
+  messagingSenderId: "751565337785",
+  appId: "1:751565337785:web:78ce58bd1543c55d75d768"
+};
